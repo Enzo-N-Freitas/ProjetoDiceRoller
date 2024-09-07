2cbf70f52600f8d69b99b8a21d596c9a8ec36d63
Enzo Negrão Freitas <enzo_negrao_freitas@outlook.com>
2024-09-06 22:26:51 -0300

    Criação do Layout + Lógica

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -5,11 +5,26 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        
     }
+    private void RollerButton_Clicked(object sender, EventArgs e)
+    {
+        if (DicePicker.SelectedItem != null)
+        {
+            int NumberOfSides = 0;
+            int Result = 0;
+            NumberOfSides = int.Parse(DicePicker.SelectedItem.ToString());
+            Random random = new Random();
+            Result = random.Next(1, NumberOfSides + 1);
+            DiceResult.Text = Result.ToString();
+        }
 
-    //Sortear um número aleatório através do clique do botão
-    //Devolver o número sorteado para a interface
-    //Conseguir selecionar a quantidade de lados a partir do Picker
+        }
 
-}
+
+        //Sortear um número aleatório através do clique do botão
+        //Devolver o número sorteado para a interface
+        //Conseguir selecionar a quantidade de lados a partir do Picker
+
+    }
 
