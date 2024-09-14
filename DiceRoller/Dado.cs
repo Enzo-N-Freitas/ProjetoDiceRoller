702a62438ebbbd66676871dfeb8a51b2632ce840
Etec Prof. Horácio Augusto da Silveira <enzo_negrao_freitas@outlook.com>
2024-09-13 22:44:33 -0300

    AboutPage Adicionada; Classe Dado criada; Lógica de quantidade de dados e modificador da rolagem criados

---
+++ DiceRoller/Dado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public class Dado
+    {
+        private int quantidadeDeLados;
+        private int modificador;
+
+        public Dado(int QuantidadeDeLados, int Modificador)
+        {
+            quantidadeDeLados = QuantidadeDeLados;
+            modificador = Modificador;
+        }
+
+        public int RolarDado() 
+        {
+             int Result = new Random().Next(1, quantidadeDeLados + 1) + modificador;
+             return Result;
+        }
+    }
+}
--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -1,30 +1,38 @@
-﻿namespace DiceRoller;
+﻿using System.Text;
+
+namespace DiceRoller;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-        
     }
+
     private void RollerButton_Clicked(object sender, EventArgs e)
     {
+        Dado dado = new Dado(int.Parse(DicePicker.SelectedItem.ToString()), int.Parse(DiceModifier.SelectedIndex.ToString()) + 1);
+
         if (DicePicker.SelectedItem != null)
         {
-            int NumberOfSides = 0;
-            int Result = 0;
-            NumberOfSides = int.Parse(DicePicker.SelectedItem.ToString());
-            Random random = new Random();
-            Result = random.Next(1, NumberOfSides + 1);
-            DiceResult.Text = Result.ToString();
+            var String = new StringBuilder();
+            int soma = 0;
+            if (DiceAmount.Text != null)
+            {
+                for (int i = 1; i <= int.Parse(DiceAmount.Text); i++)
+                {
+                    int resultado = dado.RolarDado();
+                    String.AppendLine($"{resultado}");
+                    soma += resultado;
+                }
+            }
+            DiceSum.Text = soma.ToString();
         }
-
-        }
-
-
-        //Sortear um número aleatório através do clique do botão
-        //Devolver o número sorteado para a interface
-        //Conseguir selecionar a quantidade de lados a partir do Picker
-
     }
+
+    //Sortear um número aleatório através do clique do botão
+    //Devolver o número sorteado para a interface
+    //Conseguir selecionar a quantidade de lados a partir do Picker
+
+}
 
